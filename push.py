56ca88f2042ef4124bcfcd5a0e1176a243af3fae
Jack O'Connor <oconnor663@gmail.com>
2019-04-08 13:53:11 -0400

    do an automatic git push in push.py

--- push.py
+++ push.py
@@ -16,14 +16,13 @@ def main():
         print("repo isn't clean")
         return 1
 
-    sh("git fetch").run()
+    cmd("git", "push", "origin", "master").run()
 
-    commits = sh("git log origin/master..").read()
-    if commits:
-        print("unpushed commits")
-        return 1
-
-    cmd("ssh", "jacko@jacko.io", "cd /srv/jacko.io && git pull --ff-only && peru sync --no-cache").run()
+    cmd(
+        "ssh",
+        "jacko@jacko.io",
+        "cd /srv/jacko.io && git pull --ff-only && peru sync --no-cache",
+    ).run()
 
 
 if __name__ == "__main__":
