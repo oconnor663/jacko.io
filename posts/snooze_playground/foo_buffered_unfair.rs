0790ddca3ef6c30456a2d503beb34f3fefc8486c
Jack O'Connor <oconnor663@gmail.com>
2025-12-28 20:09:52 -0800

    add an unfair mutex example

---
+++ posts/snooze_playground/foo_buffered_unfair.rs
@@ -0,0 +1,31 @@
+use futures::StreamExt;
+use futures::lock::Mutex;
+use std::sync::LazyLock;
+use tokio::time::{Duration, sleep};
+
+// This mutex implementation is unfair! (It also doesn't have a const new
+// function, so we need a `LazyLock` to initialize it.)
+static LOCK: LazyLock<Mutex<()>> = LazyLock::new(|| Mutex::new(()));
+
+async fn foo() {
+    let guard = LOCK.lock().await;
+    sleep(Duration::from_millis(10)).await;
+    drop(guard);
+    // This second sleep allows the second `foo` to acquire the lock before
+    // the first `foo` finishes below. If you comment it out, the deadlock
+    // no longer appears. (The original version of this example using a
+    // `tokio::sync::Mutex` didn't need this, because that mutex is fair.)
+    sleep(Duration::from_millis(1)).await;
+}
+
+#[tokio::main]
+async fn main() {
+    futures::stream::iter([foo(), foo()])
+        .buffered(2)
+        .for_each(async |_| {
+            println!("We make it here...");
+            foo().await;
+            println!("...but not here!");
+        })
+        .await;
+}
