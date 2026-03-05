cf8d5fe82ad2c916a2fec8492ee35e34c22e700f
Jack O'Connor <oconnor663@gmail.com>
2026-03-05 08:52:05 -0800

    make push.py more forceful

--- push.py
+++ push.py
@@ -14,22 +14,36 @@ import sys
 
 here = Path(__file__).parent
 
+SSH_BASH = """
+set -e -u -o pipefail
+
+cd /srv/jacko.io
+
+if [[ -n "$(git status --porcelain --untracked-files=no)" ]] ; then
+    echo "server repo is dirty"
+    exit 1
+fi
+
+git fetch --all
+git reset --hard origin/master
+peru sync --no-cache
+(cd render_posts && cargo run --release)
+"""
+
 
 def main():
     os.chdir(str(here))
 
-    status = cmd("git", "status", "--porcelain").read()
+    status = cmd("git", "status", "--porcelain", "--untracked-files=no").read()
     if status:
-        print("repo isn't clean")
+        print("local repo is dirty")
         return 1
 
     cmd("git", "push", "origin", "master").run()
 
-    cmd(
-        "ssh",
-        "jacko@jacko.io",
-        "cd /srv/jacko.io && git pull --ff-only && peru sync --no-cache && (cd render_posts && cargo run --release)",
-    ).run()
+    output = cmd("ssh", "jacko@jacko.io", "/usr/bin/bash").stdin_bytes(SSH_BASH).unchecked().run()
+    if output.status != 0:
+        return 1
 
 
 if __name__ == "__main__":
